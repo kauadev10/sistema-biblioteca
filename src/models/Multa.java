6851278c74088132c7ba7149f775ef03c4579322
Kauã Fagundes <kauan2542@gmail.com>
2026-03-29 20:25:17 -0300

    fix: ajustando informacoes da classe Multa

--- src/models/Multa.java
+++ src/models/Multa.java
@@ -3,11 +3,11 @@ package models;
 public class Multa {
     private int id;
     private float valor;
-    private String status; // "Pendente" ou "Paga"
+    private String status;
 
     public Multa(int id, float valor) {
         this.id = id;
         this.valor = valor;
-        this.status = "Pendente"; // Toda multa nasce pendente, certo?
+        this.status = "Pendente";
     }
 }
