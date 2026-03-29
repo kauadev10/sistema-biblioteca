27c32ba3cbbb6e464e6cfa97599074f359c59704
Kauã Fagundes <kauan2542@gmail.com>
2026-03-29 20:18:11 -0300

    fix: adicionado o construtor da classe Aluno

--- src/models/Aluno.java
+++ src/models/Aluno.java
@@ -1,5 +1,14 @@
 package models;
 
-public class Aluno extends Pessoa{
-    private String martriculaRA;
+public class Aluno extends Pessoa {
+    private String matriculaRA;
+
+    // Construtor do Aluno
+    public Aluno(int id, String cpf, String nome, int telefone, String email, String matriculaRA) {
+        // Manda os dados básicos lá para o construtor do Pai (Pessoa)
+        super(id, cpf, nome, telefone, email);
+
+        // Salva o dado que é só do Aluno
+        this.matriculaRA = matriculaRA;
+    }
 }
